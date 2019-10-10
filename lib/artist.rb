56799ec42292d636a3516f13bea764fa771e1b49
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-10 01:21:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,27 @@ class Artist
     @@all
   end
 
-  def
+  def add_song(song)
+    song.artist = self
 end
-end 
+
+  def songs
+    Song.all.selct{|song| song.artist == self}
+end
+
+def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
+end
