1498d210f45ad30ae1e84b5c39993e5d507da502
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-10 01:02:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  @@all = []
+
+  attr_accessor :name, :artist, :
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def
+end
+end 
